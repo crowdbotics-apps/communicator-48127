5d3034b072a256db5f29004d437bf13af29cda30
Crowdbotics <team@crowdbotics.com>
2024-05-27 20:33:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "communicator-48127",
+  url: "https://communicator-48127.botics.co",
+  api: "https://communicator-48127.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
